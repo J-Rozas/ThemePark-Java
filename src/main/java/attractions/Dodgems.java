785e2febcdbad1cf3afb859922a430d7adb09c49
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-20 18:28:00 +0100

    Add implementation of Interface ITicketed

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,24 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements ITicketed {
+
+    private double startingPrice;
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+        this.startingPrice = 4.50;
+    }
+
+    @Override
+    public double defaultPrice() {
+        return this.startingPrice;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        return visitor.getAge() < 12 ? defaultPrice() / 2 : defaultPrice();
     }
 }
