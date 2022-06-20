742bc2afb605ecb274d9fb1233267d5ccc75427f
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-20 18:27:52 +0100

    Add test to check implementation of Interface ITicketed

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(11, 1.40, 23.62);
+        visitor2 = new Visitor(12, 1.50, 234.00);
     }
 
 
@@ -28,5 +33,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void chargesNormalPriceOver12YearOld() {
+        assertEquals(4.50, dodgems.priceFor(visitor2), 0.0);
+    }
+
+    @Test
+    public void chargesHalfPriceOver12YearOld() {
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.0);
     }
 }
