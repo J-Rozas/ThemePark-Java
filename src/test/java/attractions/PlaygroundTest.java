2b7ccb672ac32de28f505edc6d360af4f9d835ae
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-20 17:43:37 +0100

    First commit

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(16, 1.60, 100.93);
+        visitor2 = new Visitor(9, 1.40, 1092848302938923.12);
     }
 
     @Test
@@ -27,4 +32,15 @@ public class PlaygroundTest {
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
     }
+
+    @Test
+    public void overAgeVisitorCannotEnter() {
+        assertFalse(playground.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void underAgeVisitorCanEnter() {
+        assertTrue(playground.isAllowedTo(visitor2));
+    }
+
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(60, 1.80, 102.00);
+        visitor2 = new Visitor(1, 1.00, 0.00);
     }
 
     @Test
@@ -27,5 +32,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void overAgeVisitorCanEnter() {
+        assertTrue(tobaccoStall.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void underAgeVisitorCannotEnter() {
+        assertFalse(tobaccoStall.isAllowedTo(visitor2));
     }
 }
