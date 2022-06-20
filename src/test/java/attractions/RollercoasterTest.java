933667d3b2f7016cdb8fe90ae053d80c722cb481
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-20 18:14:58 +0100

    Add test to check implementation of ITicketed

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -9,14 +9,19 @@ import static org.junit.Assert.*;
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
-    Visitor visitor1 = new Visitor(30, 1.78, 39.93);
-    Visitor visitor2 = new Visitor(1, 1.78, 38.19);
-    Visitor visitor3 = new Visitor(30, 1.10, 38.19);
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
 
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(30, 1.78, 39.93);
+        visitor2 = new Visitor(1, 1.78, 38.19);
+        visitor3 = new Visitor(30, 1.10, 38.19);
+        visitor4 = new Visitor(40, 5.98, 63.00);
     }
 
     @Test
@@ -45,7 +50,17 @@ public class RollercoasterTest {
     }
 
     @Test
-    public void doesNotAccepOldEnoughAndNotTallEnough() {
+    public void doesNotAcceptOldEnoughAndNotTallEnough() {
         assertFalse(rollerCoaster.isAllowedTo(visitor3));
+    }
+
+    @Test
+    public void chargesNormalFeeIfNoSpecialConditions() {
+        assertEquals(8.40, rollerCoaster.priceFor(visitor1), 0.00);
+    }
+
+    @Test
+    public void chargesDoubleFeeIfTall() {
+        assertEquals(16.80, rollerCoaster.priceFor(visitor4), 0.00);
     }
 }
