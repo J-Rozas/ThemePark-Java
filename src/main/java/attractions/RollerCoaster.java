0d0fe9676401774b4db3e2ad3a0b8a31e75c7466
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-20 18:17:53 +0100

    Implement ITicketed

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,16 +1,30 @@
 package attractions;
 
 import behaviours.ISecurity;
+import behaviours.ITicketed;
 import people.Visitor;
 
-public class RollerCoaster  extends Attraction implements ISecurity {
+public class RollerCoaster  extends Attraction implements ISecurity, ITicketed {
+
+    private double startingPrice;
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+        this.startingPrice = 8.40;
     }
 
     @Override
     public boolean isAllowedTo(Visitor visitor) {
         return visitor.getAge() >= 12 && visitor.getHeight() >= 1.45;
+    }
+
+    @Override
+    public double defaultPrice() {
+        return this.startingPrice;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        return visitor.getHeight() > 2.00 ? defaultPrice() * 2 : defaultPrice();
     }
 }
